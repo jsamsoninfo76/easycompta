9467350a053cec5409bb0c30d3bbf6d0d2903f94
jeremie.samson76@gmail.com <jeremie.samson76@gmail.com@8fd65c6f-2094-dc01-a221-0dce0ce1fb5a>
2013-06-17 20:54:43 +0000

    ajout test add

---
+++ branches/script/include/configuration/configuration.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = array(
+	"host" => "mysql5-15.perso",
+	"user" => "jeremiesfolio",
+	"pass" => "Nap1804",
+	"port" => "3306", 
+	"db"   => "jeremiesfolio"
+);
+
+?>
---
+++ branches/script/index.php
@@ -0,0 +1,7 @@
+<?php
+
+include("include/include.php");
+
+
+
+?>
